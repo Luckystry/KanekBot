f60fcd36a01d9d8a9dbab98afeaaa7168d62dca2
Luckystry <30297901+Luckystry@users.noreply.github.com>
2018-01-30 15:56:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -22,11 +22,18 @@ bot.on('message', function (message) {
     }
   })
 
+  
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - *regles \n - *new");
-    }
 
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+            .SetColor('#01FEDC')
+            .addField("Commande", "    -/help: Affiche les commandes mis aà votre disposotion")
+        message.channel.sendEmbed(help_embed);
+        //message.reply("On est pas a Villeurbanne ici")
+        console.log("Commande help effectué");
+
+    }
 
     if (message.content === "wesh"){
         message.reply("On est pas a Villeurbanne ici")
