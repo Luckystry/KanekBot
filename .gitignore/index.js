548197a2b1b7935fe70669a8337e0d7fa6ec3db8
Luckystry <30297901+Luckystry@users.noreply.github.com>
2018-02-01 15:40:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("/")
+var prefix = ("*")
+
+db.defaults({ histoires: [], xp: []}).write
 
 bot.on('ready', function() {
     bot.user.setGame("Créateur: Nox");
@@ -11,32 +13,52 @@ bot.on('ready', function() {
 bot.login("NDA2Mzg2NTUwOTY4OTQyNTky.DVIIBw.4fYscZ7Fb_9J1LIzgFoatWABApQ");
 
 bot.on('message', function (message) {
-    if (message.content === '/regles') {
+    if (message.content === '*regles') {
         message.channel.send(">Ne pas envoyer pas de photo pédopornographique dans les salons du serveur. Vérifiez les sources de vos photos pour éviter tout problème \r >Respecter les lois en vigueurs en France et dans vos pays respectif. \r >Ne pas harceler, rabaisser et ne pas divulguer d’information sur les personnes sans leurs accords. \r >Ne pas spam et évitez les doubles postes. \r >Respectez les salons et leurs contenues. \r >Respectez les autres. \r >Ne pas demander de rôles. \r >Les pubs sont totalement interdites. \r >Le troll est interdit.")
     }
   })
 
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb)
+        console.log(userxp);
+        console.log(`Nombre d'xp : ${userxp[1]}`)
+
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+
+    }
+
 bot.on('message', function (message) {
-    if (message.content === '/new') {
-        message.channel.send("Pour les nouveaux vous allez voir que vous pouvez choisir vos rôles et accepter les règles dans #vos-role et #acceptez-le-reglement-pour-rejoindre-le-serveur .Par exemples H en réaction c'est homme et F en réaction c'est Femme et le signe vert pour accepter les règles, vous avez juste a cliquer sur la bonne réaction et ainsi de suite.")
+    if (message.content === '*new') {
+        message.channel.send("Pour les nouveaux vous allez voir que vous pouvez choisir vos rôles par exemples H en réaction c'est homme et F en réaction c'est Femme, vous avez juste a cliquer sur la bonne réaction et ainsi de suite.")
     }
   
     if (message.channel.id === '408191248256598016') {
         if (isNaN(message.content)) {
             message.delete()
-            message.author.send("Merci de lire les règles, il n'y a pas besoin d'écrire ici ;-)")
-    } 
-}
-    
-       if (message.channel.id === '407993050929823765') {
-        if (isNaN(message.content)) {
-            message.delete()
-            message.author.send("Merci de lire les règleset de cliquer sur la réaction déjà mise, il n'y a pas besoin d'écrire ici ;-)")
+            message.author.send("Merci de lire les règles, il n'y a pas besoin d'écrire ;-)")
     } 
 }
 
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes: \n - *regles, \n - *new");
+    }
+
+    if (message.content === prefix + "xpstat"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle('xp de ${message.author}')
+            .setDescription("Voici tout tes xp")
+            .addField("xp :", '${xpfinal[1]} xp')
+        message.channel.send({embed: xp_embed});
     }
 
     if (message.content === "wesh"){
@@ -82,3 +104,4 @@ bot.on('message', function (message) {
     }
 
 })
+    
