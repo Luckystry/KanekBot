a5d0c259ce0b203ad1cee61818657aa1b1d4b31f
Luckystry <30297901+Luckystry@users.noreply.github.com>
2018-02-06 11:04:21 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,31 +1,30 @@
-    const Discord = require('discord.js');
-    const bot = new Discord.Client();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-    var prefix = ("*")
+var prefix = ("*")
 
-    bot.on('ready', function() {
-        bot.user.setGame("Créateur: Nox");
-        console.log("Connectedç");
-    });
+bot.on('ready', function() {
+    bot.user.setGame("Créateur: Nox");
+    console.log("Connectedç");
+});
 
-    bot.login("NDA2Mzg2NTUwOTY4OTQyNTky.DVIIBw.4fYscZ7Fb_9J1LIzgFoatWABApQ");
+bot.login("NDA2Mzg2NTUwOTY4OTQyNTky.DVIIBw.4fYscZ7Fb_9J1LIzgFoatWABApQ");
 
-    bot.on('message', function (message) {
-        if (message.content === '*regles') {
-            message.channel.send("***-IL EST IMPÉRATIF*** de respecter chaque chanel. Toute actions entreprise dans le mauvais chanel débouchera sur un mute de 5 à 10 minutes et 1 avertissement(warn). \r -Au bout de ***3*** avertissements (warn) un kick du serveur sera effectuée. Au bout de 5 avertissements(warn) c'est un ban définitif du serveur. Pour suppression de warn il faut ***1 semaine*** sans aucune erreur. \r -Le respect d'autrui est ***PRIMORDIALE***. Toute insultes, menaces, harcèlement, incitation à la haine ou à la violence serra puni d'un kick immédiat du serveur. Toute récidives se verra ban définitivement avec possibilité de de- bannissement au bout de 2 jours. \r -***TOUT*** manquement de respect à un membre du staff sera puni d'un avertissement(warn).")
-        }
-    })
-
-    
-        if (message.channel.id === '408191248256598016') {
-            if (isNaN(message.content)) {
-                message.delete()
-                message.author.send("Merci de lire les règles, il n'y a pas besoin d'écrire :wink:")
-        } 
+bot.on('message', function (message) {
+    if (message.content === '*regles') {
+        message.channel.send("***-IL EST IMPÉRATIF*** de respecter chaque chanel. Toute actions entreprise dans le mauvais chanel débouchera sur un mute de 5 à 10 minutes et 1 avertissement(warn). \r -Au bout de ***3*** avertissements (warn) un kick du serveur sera effectuée. Au bout de 5 avertissements(warn) c'est un ban définitif du serveur. Pour suppression de warn il faut ***1 semaine*** sans aucune erreur. \r -Le respect d'autrui est ***PRIMORDIALE***. Toute insultes, menaces, harcèlement, incitation à la haine ou à la violence serra puni d'un kick immédiat du serveur. Toute récidives se verra ban définitivement avec possibilité de de- bannissement au bout de 2 jours. \r -***TOUT*** manquement de respect à un membre du staff sera puni d'un avertissement(warn).")
     }
 
-        if (message.content === prefix + "help"){
-            message.channel.sendMessage("Liste des commandes: \n - *regles, \n - *new");
-        }
 
-    })  
+    if (message.channel.id === '408191248256598016') {
+        if (isNaN(message.content)) {
+            message.delete()
+            message.author.send("Merci de lire les règles, il n'y a pas besoin d'écrire :wink:")
+    } 
+}
+
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n - *regles, \n - *new");
+    }
+
+})
